2103205ba1f6e8fa821c68665a88813fedf377b6
Harde Khalis <hardekhalis22@yahoo.COM>
2026-09-01 01:33:55 +0300

    fix railway mysql connection

--- server.js
+++ server.js
@@ -42,11 +42,11 @@ app.use(express.static(publicPath));
 // ==========================================
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT) || 3306,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 
     waitForConnections: true,
     connectionLimit: 10,
